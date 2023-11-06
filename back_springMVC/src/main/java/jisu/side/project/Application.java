10d12fe0a0dc990cb4024d48e1204f30f03a8566
chou <asha.jisu@gmail.com>
2023-11-06 23:11:47 +0900

    security & index

---
+++ back_springMVC/src/main/java/jisu/side/project/Application.java
@@ -0,0 +1,13 @@
+package jisu.side.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ back_springMVC/src/main/java/jisu/side/project/security/SecurityConfig.java
@@ -0,0 +1,83 @@
+package jisu.side.project.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import jakarta.servlet.DispatcherType;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * 스프링 부트 2.7.0 이상의 버전을 사용하면 스프링 시큐리티 5.7.0 혹은 이상의 버전과 의존성이 있습니다.
+ * 그렇다면 WebSecurityConfigurerAdapter가 deprecated되며
+ * 스프링 부트 3 이상의 버전과 의존관계인 스프링 시큐리티 6 이상에서는 클래스가 제거되었음
+ *
+ * 참고 https://covenant.tistory.com/277
+ * */
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login -> login
+//                        .loginPage("/view/login")
+//                        .loginProcessingUrl("/login/process")
+                        .usernameParameter("userid")
+                        .passwordParameter("pw")
+                        .defaultSuccessUrl("/main/second", true)
+                        .permitAll()
+                )
+                .logout(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("userid")
+                .password("pw")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /** 이전버전
+     *     @Bean
+     *     public PasswordEncoder passwordEncoder() {
+     *         return new BCryptPasswordEncoder();
+     *     }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("userid")
+                .password(passwordEncoder().encode("pw"))
+                .roles("USER");
+    }
+
+     변경후
+    AuthenticationManager 생성시 UserSecurityService와 PasswordEncoder가 자동으로 설정
+    */
+}
