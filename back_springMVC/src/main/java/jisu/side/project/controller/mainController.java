f2103d23a3c0738f72bdbed2ded4e37aeec5a027
chou <asha.jisu@gmail.com>
2023-11-06 23:16:35 +0900

    security & index

---
+++ back_springMVC/src/main/java/jisu/side/project/controller/mainController.java
@@ -0,0 +1,26 @@
+package jisu.side.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * view resolver 작동 확인
+ *  @2023.11.6
+ *
+ *  page List
+ *  "/" resources/static/index.html 권한불필요
+ *  "/main/second" webapp/view/second.html 로그인필요
+ *
+ * */
+@Controller
+@RequestMapping("/main")
+public class mainController {
+
+    /** 로그인 성공화면
+     * */
+    @GetMapping("/second")
+    public String second(){
+        return "second";
+    }
+
+}
