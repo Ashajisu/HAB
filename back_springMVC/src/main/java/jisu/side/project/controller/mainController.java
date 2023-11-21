1606e02eddb43e9194eaa53ae20c96b0787824af
jisu <jisu_um@arisys.co.kr>
2023-11-21 17:15:04 +0900

    FIX view prefix

--- back_springMVC/src/main/java/jisu/side/project/controller/mainController.java
+++ back_springMVC/src/main/java/jisu/side/project/controller/mainController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * view resolver 작동 확인
  *  @2023.11.6
+ *  @2023.11.21
  *
  *  page List
  *  "/" resources/static/index.html 권한불필요
- *  "/main/second" webapp/view/second.html 로그인필요
- *
+ *  "/main/second" webapp/view/second.html 로그인필요 :
+ *      spring.mvc.view.prefix=/WEB-INF/views/
+ *      spring.mvc.view.suffix=.html
  * */
 @Controller
 @RequestMapping("/main")
@@ -20,6 +22,7 @@ public class mainController {
      * */
     @GetMapping("/second")
     public String second(){
+//        return "/second.html";
         return "second";
     }
 
