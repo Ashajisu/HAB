5c102a9cce4229d304e599a2fb6e157d2d0c10a3
chou <asha.jisu@gmail.com>
2024-01-03 01:30:34 +0900

    fix index.js feat Navigation Guard permit check
    permitAll > isLoging > User > Admin
    caution! next() have to run just one time

--- pront/src/router/index.js
+++ pront/src/router/index.js
@@ -48,42 +48,32 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   //meta 속성에 정의된 title 값을 사용하여 브라우저의 제목을 동적으로 변경하는 역할을 합니다.
   document.title = to.meta.title;
-  if(!to.matched.some(record => record.meta.permitAll) && !isLoging()){
-    next({ path: '/home/login'});
-  }else{
+  if(to.matched.some(record => record.meta.permitAll)){ //로그인 불필요
     next();
-  }
+  }else if(!isLoging()){ //로그인 필요
+    next({ path: '/home/login'}); 
 
-    //children으로 권한 상속되는 구조에서 사용
-    //상위 라우터부터 현재까지 권한 확인 (하나라도 권한 필요시 상속으로 권한 확인함)
-    //true, false 상관없이 메타필드가 있으면 권한 확인 
-  if (to.matched.some(record => record.meta.requiresAdmin)) {
-    if (!isAdmin()) {
-      // ADMIN 권한이 없으면 홈으로 리다이렉트
-      next({ path: '/' });
-    } else {
-      next(); // 권한이 있으면 계속 진행
-    }
-  } else if (to.matched.some(record => record.meta.requiresUser)) {
-    if (!isUser()) {
+  //로그인 이후 권한 필요
+  //children으로 권한 상속되는 구조에서 사용 : 상위 라우터부터 현재까지 하나라도 필요하면 권한 확인함
+  //true, false 상관없이 메타필드가 있으면 권한 확인 
+  } else if (to.matched.some(record => record.meta.requiresUser)&&!isUser()) {
       // USER 권한이 없으면 홈으로 리다이렉트
-      next({ path: '/' });
-    } else {
-      next(); // 권한이 있으면 계속 진행
-    }
+      next({ path: '/home' });
+  } else if (to.matched.some(record => record.meta.requiresAdmin) && !isAdmin()) {
+      // ADMIN 권한이 없으면 홈으로 리다이렉트
+      next({ path: '/home' });
   } else {
-    next(); // 권한이 필요하지 않은 페이지는 계속 진행
-  }
-
-  next();
+      next();
+  }  
 });
+
 function isLoging(){
   console.log("isLoging?");
-  return false;
+  return true;
 }
 function isAdmin(){
   console.log("isAdmin?");
-  return true;
+  return false;
 }
 function isUser(){
   console.log("isUser?");
